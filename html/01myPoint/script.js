a9e66773fc7d621b42c45af0008bf3d190a01491
JiKon <canyounotspammethanks@gmail.com>
2020-03-27 11:59:05 +0100

    03draggablePoint

--- html/01myPoint/script.js
+++ html/01myPoint/script.js
@@ -8,3 +8,16 @@ canvas.width = width;
 canvas.height = height;
 
 // begin hier met jouw code voor deze opdracht
+
+let colors = ["grey", "black", "red", "green", "yellow", "purple"]
+
+for(let i = 0; i <100; i++){
+    let x = getRandomInt(0, width);
+    let y = getRandomInt(0, height);
+    let radius = getRandomInt(10, 50);
+    let color = colors[getRandomInt(0, colors.length)];
+
+
+    let myPoint = new Point(x, y, radius, color);
+    myPoint.draw(context);
+}
