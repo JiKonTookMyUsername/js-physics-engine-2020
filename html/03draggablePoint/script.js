56f198864e6d9fcc815394f80249b48b51ad7ee6
JiKon <canyounotspammethanks@gmail.com>
2020-04-05 14:32:46 +0200

    Finished draggablePoint
    You can now drag the point on the canvas by clicking your linker mousebutton

--- html/03draggablePoint/script.js
+++ html/03draggablePoint/script.js
@@ -8,9 +8,19 @@ canvas.width = width;
 canvas.height = height;
 
 
-let myPoint = new Point(200,200,50,"black")
+// mousemove geeft hij de coördinaten aan van de muis (de X en Y in dit geval)
+// mousedown geeft hij de coördinaten aan van het punt waar geklikt is met de linker muisknop (de X en Y in dit geval)
+// => noemen we een arrow-function
+
+// addEventListener('mousemove',(evt) => {
+//   console.log('de x is ' + evt.clientX);
+//   console.log('de y is ' + evt.clientY);
+// });
+
+let myPoint = new Point(200,300,30,"black",true);
+
 function animate(){
-  context.clearRect(0,0,width,height);
+  context.clearRect(0,0,width,height)
   myPoint.draw(context)
 }
 
